7f4f352862102d8e2521bc5d22d74fae19b47867
karajen2 <skpkarthik@gmail.com>
2016-03-17 23:32:05 +0530

    changes added in About.cshtml

---
+++ FoodApplication/FoodApplication/Models/HotelReview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodApplication.Models
+{
+    public class HotelReview
+    {
+        public string Name { get; set; }
+        public int Rating { get; set; }
+    }
+}
